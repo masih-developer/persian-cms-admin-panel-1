3bfa7b29c37bfd1cc9fc2f47603eabd13966ae0b
masih abedini <masih7816@gmail.com>
2023-05-03 21:49:45 +0330

    create theme file and edit index css file

---
+++ src/theme.js
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "Lalezar-Regular",
+        h1: {
+            fontSize: 36,
+        },
+        h2: {
+            fontSize: 30,
+        },
+        h3: {
+            fontSize: 24,
+        },
+        h4: {
+            fontSize: 18,
+        },
+        h5: {
+            fontSize: 16,
+        },
+        h6: {
+            fontSize: 14,
+        },
+    },
+});
+
+export default theme;
