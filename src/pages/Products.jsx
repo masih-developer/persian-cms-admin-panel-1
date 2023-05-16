88f34aae9b337e48f3a6f5ad1cb58f733427ec4e
masih abedini <masih7816@gmail.com>
2023-05-17 00:10:39 +0330

    refactor codes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,12 +5,16 @@ import { useEffect, useState } from "react";
 
 const Products = () => {
     const [allProducts, setAllProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const getAllProducts = () => {
         fetch("http://localhost:3000/api/products")
             .then((res) => res.json())
             .then((products) => {
                 setAllProducts(products);
+            })
+            .finally(() => {
+                setIsLoading(false);
             });
     };
 
@@ -21,7 +25,11 @@ const Products = () => {
     return (
         <Box>
             <AddNewProduct getAllProducts={getAllProducts} />
-            <ProductsTable allProducts={allProducts} getAllProducts={getAllProducts} />
+            <ProductsTable
+                allProducts={allProducts}
+                getAllProducts={getAllProducts}
+                isLoading={isLoading}
+            />
         </Box>
     );
 };
